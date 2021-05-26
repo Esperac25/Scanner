eadf62169d311cbb42444d6185077b890eafd8b9
Esperac25 <esperac25@gmail.com>
2021-05-26 10:53:16 -0700

    EC: added exception handling for divison class

--- src/main/java/com/qa/scanner/Calculator.java
+++ src/main/java/com/qa/scanner/Calculator.java
@@ -1,48 +1,56 @@
 package com.qa.scanner;
 
+
+
 public class Calculator {
 
-	public static double calc(String m, int i, int j) {
+	public static double calc(String m, double n1, double n2) {
 		if (m == "Addition") {
-			return add(i, j);
+			return add(n1, n2);
 		} else if (m == "Subtraction") {
-			return sub(i, j);
+			return sub(n1, n2);
 		} else if (m == "Multiplication") {
-			return multi(i, j);
+			return multi(n1, n2);
 		} else {
-			return div(i, j);
+			return div(n1,n2);
+			
 		}
+		
 	}
 
-	public static int add(int i, int j) {
+	public static double add(double i, double j) {
 		return i + j;
 	}
 
-	public static int sub(int i, int j) {
-		return i - j;
+	public static double sub(double n1, double n2) {
+		return n1 - n2;
 
 	}
 
-	public static int multi(int i, int j) {
-		return i * j;
+	public static double multi(double n1, double n2) {
+		return n1 * n2;
 	}
 
-	public static double div(int i, int j) throws IllegalArgumentException {
-//		 try { 
-//			 	double s = i/j;
-//	           System.out.println(s);
-//	        } catch(IllegalArgumentException e) {
-//	        	if(i == 0 || j == 0) {
-//	        		throw new IllegalArgumentException("Error: cannot divide by 0");
-//	        	}
-//	        	else if(i !== (int i) || ) {
-//	        		throw new IllegalArgumentException("Error: type mismatched, please enter a number");
-//	        	} else {
-//	        		System.out.println(e);
-//	        	}
-		return i/j;
-	        } 
-		
+	public static double div(double i, double j) {
+		try {
+			return i/j;
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		} 
+		return 0;	
+	} 
+//	public static void divException(double i, double j){
+//		try {
+//			if(i == 0 || j == 0) {
+//				System.out.print("Error: cannot divide by 0");
+//				System.out.print(div(i, j));
+//			}
+//			
+//		} catch (InputMismatchException e) {
+//			System.out.println("Error: input must be a number");
+//			System.out.println(e);
+//		}
+//	}
 
 	}
 
--- src/main/java/com/qa/scanner/Runner.java
+++ src/main/java/com/qa/scanner/Runner.java
@@ -8,42 +8,55 @@ public class Runner {
 	public static void main(String[] arg) {
 
 		boolean running = true;
-		do {
+		while (running) {
 			System.out.println("Welcome to the Java calculator\n\t" + "Please enter a number from the following:\n\t"
 					+ "1: Addition\n\t" + "2: Subtraction\n\t" + "3: Division\n\t" + "4: Multiplication\n\t"
 					+ "5: Close Calculator");
-			int res = Integer.parseInt(mathScanner.nextLine());// parses the string comes back as an integer
+			int res = 0;
+			if(mathScanner == null) {
+				res = 5;
+			} 
+			try{res = mathScanner.nextInt();
+			}catch(Exception e){
+				System.out.println(e);
+			}
 
 			switch (res) {
 			case 1:
 				print("Addition");
-				break;
+				continue;
 			case 2:
 				print("Subtraction");
-				break;
+				continue;
 			case 3:
 				print("Division");
-				break;
+				continue;
 			case 4:
 				print("Multiplication");
-				break;
+				continue;
 			case 5:
 				System.out.println("Application has closed");
 				running = false;
-				break;
+				continue;
 			default:
 				running = false;
 				break;
 			}
-		} while (running);
-
-		mathScanner.close();
+			mathScanner.close();
+		}
 	}
+	
+	
 
 	public static void print(String m) {
 		System.out.println(m + ": Enter numbers below");
-		int n1 = Integer.parseInt(mathScanner.nextLine());
-		int n2 = Integer.parseInt(mathScanner.nextLine());
-		System.out.println("= " + Calculator.calc(m, n1, n2));
+		try {
+			double n1 = mathScanner.nextDouble();
+			double n2 = mathScanner.nextDouble();
+			System.out.println("= " + Calculator.calc(m, n1, n2));
+		} catch (Exception e) {
+			System.out.println("Exception cannot execute because: " + e);
+		} 
+		mathScanner.nextLine();
 	}
 }
